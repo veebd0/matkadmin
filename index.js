349848b9efe3d269acf5b2e2739302217601b41d
Andres Järviste <andres.jarviste@gmail.com>
2020-10-08 19:45:59 +0300

    api matkade aadress

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+const veebirakendus = express();
+
+function tagastaMatkad(req, res) {
+	let matkaAndmed = [
+		{
+			pildiUrl: './assets/syst1.jpg',
+			nimi: 'Süstamatk Võhandu jõel',
+			lyhikirjeldus: 'alsdj alskdjfalsdkjf alskdjf lasjdf laskjdf lasdfasdfljasdfa. DFASdf',
+		},
+		{
+			pildiUrl: './assets/syst2.jpg',
+			nimi: 'Süstamatk Pärnu jõel',
+			lyhikirjeldus: 'alsdj alskdjfalsdkjf alskdjf lasjdf laskjdf lasdfasdfljasdfa. DFASdf',
+		},
+		{
+			pildiUrl: './assets/matk_tartus1.jpg',
+			nimi: 'Tutvume Tartu linnaga',
+			lyhikirjeldus: 'alsdj alskdjfalsdkjf alskdjf lasjdf laskjdf lasdfasdfljasdfa. DFASdf',
+		},
+	];
+
+	return res.send({
+		matkad: matkaAndmed
+	});
+}
+
+veebirakendus
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/matkad',tagastaMatkad)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
